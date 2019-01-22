ad10a51a9909198fce097c4a01c8f8f6bdfe13f5
ChelseaW <c.winfrey93@yahoo.com>
2019-01-22 16:51:55 -0600

    BasicsReview

---
+++ BasicsReview.py
@@ -0,0 +1,44 @@
+
+def main():
+     problem1()
+    # problem2()
+    # problem3()
+    # problem4()
+
+# Create a random number. Print the number.
+#
+# Hint:
+# import random
+# randomNumber = random.randint(0,9)
+
+
+# def problem1():
+    # import random
+    # randomNum = random.randint(0,20)
+    # print(randomNum)
+    # userInput =  ""
+    # while (randomNum):
+
+#
+
+
+def problem2():
+
+# Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
+
+   userInput = input("")
+    userInput = input("")
+
+    while userInput != quit
+        userInput =input("Enter a string:")
+        userInput2 = input("Try Again enter ")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
